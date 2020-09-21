808eac2073e6a144610d19806431b26f70ed2196
Tom Bassett <tbassett44@gmail.com>
2020-09-21 13:00:58 -0600

    casting22329

--- src/android/Screenshot.java
+++ src/android/Screenshot.java
@@ -1,26 +1,16 @@
 /**
  * Copyright (C) 2012 30ideas (http://30ide.as)
  * MIT licensed
- * 
+ *
  * @author Josemando Sobral
  * @created Jul 2nd, 2012.
  * improved by Hongbo LU
  */
 package com.darktalker.cordova.screenshot;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.net.Uri;
@@ -29,154 +19,244 @@ import android.util.Base64;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.util.DisplayMetrics;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class Screenshot extends CordovaPlugin {
-	
-	private TextureView findXWalkTextureView(ViewGroup group) {
+    private CallbackContext mCallbackContext;
+    private String mAction;
+    private JSONArray mArgs;
 
-		int childCount = group.getChildCount();
-		for(int i=0;i<childCount;i++) {
-			View child = group.getChildAt(i);
-			if(child instanceof TextureView) {
-				String parentClassName = child.getParent().getClass().toString();
-				boolean isRightKindOfParent = (parentClassName.contains("XWalk"));
-				if(isRightKindOfParent) {
-					return (TextureView) child;
-				}
-			} else if(child instanceof ViewGroup) {
-				TextureView textureView = findXWalkTextureView((ViewGroup) child);
-				if(textureView != null) {
-					return textureView;
-				}
-			}
-		}
-		
-		return null;
-	}
-	
-	private Bitmap getBitmap() {
-		Bitmap bitmap = null;
-        View view = cordova.getActivity().getWindow().getDecorView().getRootView();
-        view.setDrawingCacheEnabled(true);
-        bitmap = Bitmap.createBitmap(view.getDrawingCache());
-        view.setDrawingCacheEnabled(false);
-		
-		return bitmap;
-	}
 
-    private void scanPhoto(String imageFileName)
-    {
+    private String mFormat;
+    private String mFileName;
+    private Integer mQuality;
+
+    protected final static String[] PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    public static final int PERMISSION_DENIED_ERROR = 20;
+    public static final int SAVE_SCREENSHOT_SEC = 0;
+
+    @Override
+    public Object onMessage(String id, Object data) {
+        if (id.equals("onGotXWalkBitmap")) {
+            Bitmap bitmap = (Bitmap) data;
+            if (bitmap != null) {
+                if (mAction.equals("saveScreenshot")) {
+                    saveScreenshot(bitmap, mFormat, mFileName, mQuality);
+                } else if (mAction.equals("getScreenshotAsURI")) {
+                    getScreenshotAsURI(bitmap, mQuality);
+                }
+            }
+        }
+        return null;
+    }
+
+    private Bitmap getBitmap() {
+        Bitmap bitmap = null;
+
+        boolean isCrosswalk = false;
+        try {
+            Class.forName("org.crosswalk.engine.XWalkWebViewEngine");
+            isCrosswalk = true;
+        } catch (Exception e) {
+        }
+
+        if (isCrosswalk) {
+            webView.getPluginManager().postMessage("captureXWalkBitmap", this);
+        } else {
+            View view = webView.getView();//.getRootView();
+            view.setDrawingCacheEnabled(true);
+            bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+        }
+
+        return bitmap;
+    }
+
+    private void scanPhoto(String imageFileName) {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(imageFileName);
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.cordova.getActivity().sendBroadcast(mediaScanIntent);
     }
-	
-	
-	@Override
-	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-	 	// starting on ICS, some WebView methods
-		// can only be called on UI threads
-		// DisplayMetrics dm = this.cordova.getActivity().getResources().getDisplayMetrics(); 
-		// final Integer densityDpi = dm.densityDpi;
-		if (action.equals("saveScreenshot")) {
-			final String format = (String) args.get(0);
-			final Integer quality = (Integer) args.get(1);
-			final String fileName = (String)args.get(2);
-			//final JSONObject crop = (JSONObject)args.optJSONObject(3);
 
-			super.cordova.getActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					
-					try {
-						if(format.equals("png") || format.equals("jpg")){
-							Bitmap bitmap = getBitmap();
-							// final Integer top = (Integer) (crop.getInt("top") * densityDpi);
-							// final Integer left = (Integer) (crop.getInt("left") * densityDpi);
-							// final Integer width = (Integer) (crop.getInt("width") * densityDpi);
-							// final Integer height = (Integer) (crop.getInt("height") * densityDpi);
-							// Bitmap resizedbitmap=Bitmap.createBitmap(bitmap,crop.getInt("top"),crop.getInt("left"),crop.getInt("width"), crop.getInt("height"));
-							// Bitmap resizedbitmap=Bitmap.createBitmap(bitmap,left,top,width,height);
-							Bitmap resizedbitmap=Bitmap.createBitmap(bitmap,0,240,2472,1206);
-							File folder = new File(Environment.getExternalStorageDirectory(), "Pictures");
-							if (!folder.exists()) {
-								folder.mkdirs();
-							}
+    private void saveScreenshot(Bitmap bitmap, String format, String fileName, Integer quality) {
+        try {
+            File folder = new File(Environment.getExternalStorageDirectory(), "Pictures");
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
 
-							File f = new File(folder, fileName + "."+format);
+            File f = new File(folder, fileName + "." + format);
 
-							FileOutputStream fos = new FileOutputStream(f);
-							if(format.equals("png")){
-								resizedbitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-							}
-							if(format.equals("jpg")){
-								resizedbitmap.compress(Bitmap.CompressFormat.JPEG, quality == null?100:quality, fos);
-							}
-							JSONObject jsonRes = new JSONObject();
-							jsonRes.put("filePath",f.getAbsolutePath());
-				                        PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
-				                        callbackContext.sendPluginResult(result);
+            FileOutputStream fos = new FileOutputStream(f);
+            if (format.equals("png")) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            } else if (format.equals("jpg")) {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality == null ? 100 : quality, fos);
+            }
+            JSONObject jsonRes = new JSONObject();
+            jsonRes.put("filePath", f.getAbsolutePath());
+            PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
+            mCallbackContext.sendPluginResult(result);
 
-                            				scanPhoto(f.getAbsolutePath());
-						}else{
-							callbackContext.error("format "+format+" not found");
+            scanPhoto(f.getAbsolutePath());
+            fos.close();
+        } catch (JSONException e) {
+            mCallbackContext.error(e.getMessage());
 
-						}
+        } catch (IOException e) {
+            mCallbackContext.error(e.getMessage());
 
-					} catch (JSONException e) {
-						callbackContext.error(e.getMessage());
-						
-					} catch (IOException e) {
-						callbackContext.error(e.getMessage());
-						
-					}
-				}
-			});
-			return true;
-		}else if(action.equals("getScreenshotAsURI")){
-			final Integer quality = (Integer) args.get(0);
-			
-			super.cordova.getActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						Bitmap bitmap = getBitmap();
+        }
+    }
 
-						ByteArrayOutputStream jpeg_data = new ByteArrayOutputStream();
-						
-						if (bitmap.compress(CompressFormat.JPEG, quality, jpeg_data)) {
-						   byte[] code = jpeg_data.toByteArray();
-						   byte[] output = Base64.encode(code, Base64.NO_WRAP);
-						   String js_out = new String(output);
-						   js_out = "data:image/jpeg;base64," + js_out;
-						   JSONObject jsonRes = new JSONObject();
-						   jsonRes.put("URI", js_out);
-				                   PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
-				                   callbackContext.sendPluginResult(result);
-							
-						   js_out = null;
-						   output = null;
-						   code = null;
-						}
-						
-						jpeg_data = null;
+    private void getScreenshotAsURI(Bitmap bitmap, int quality) {
+        try {
+            ByteArrayOutputStream jpeg_data = new ByteArrayOutputStream();
 
-					} catch (JSONException e) {
-						callbackContext.error(e.getMessage());
-						
-					} catch (Exception e) {
-						callbackContext.error(e.getMessage());
-						
-					}
-				}
-			});
+            if (bitmap.compress(CompressFormat.JPEG, quality, jpeg_data)) {
+                byte[] code = jpeg_data.toByteArray();
+                byte[] output = Base64.encode(code, Base64.NO_WRAP);
+                String js_out = new String(output);
+                js_out = "data:image/jpeg;base64," + js_out;
+                JSONObject jsonRes = new JSONObject();
+                jsonRes.put("URI", js_out);
+                PluginResult result = new PluginResult(PluginResult.Status.OK, jsonRes);
+                mCallbackContext.sendPluginResult(result);
 
-			return true;		
-		}
-		callbackContext.error("action not found");
-		return false;
-	}
+                js_out = null;
+                output = null;
+                code = null;
+            }
+
+            jpeg_data = null;
+
+        } catch (JSONException e) {
+            mCallbackContext.error(e.getMessage());
+
+        } catch (Exception e) {
+            mCallbackContext.error(e.getMessage());
+
+        }
+    }
+
+    public void saveScreenshot() throws JSONException{
+        mFormat = (String) mArgs.get(0);
+        mQuality = (Integer) mArgs.get(1);
+        mFileName = (String) mArgs.get(2);
+
+        super.cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mFormat.equals("png") || mFormat.equals("jpg")) {
+                    Bitmap bitmap = getBitmap();
+                    Bitmap resizedbitmap=Bitmap.createBitmap(bitmap,0,240,2472,1206);
+                    if (bitmap != null) {
+                        saveScreenshot(resizedbitmap, mFormat, mFileName, mQuality);
+                    }
+                } else {
+                    mCallbackContext.error("format " + mFormat + " not found");
+
+                }
+            }
+        });
+    }
+
+    public void getScreenshotAsURI() throws JSONException{
+        mQuality = (Integer) mArgs.get(0);
+
+        super.cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = getBitmap();
+                if (bitmap != null) {
+                    getScreenshotAsURI(bitmap, mQuality);
+                }
+            }
+        });
+    }
+
+     public void getScreenshotAsURISync() throws JSONException{
+        mQuality = (Integer) mArgs.get(0);
+        
+        Runnable r = new Runnable(){
+            @Override
+            public void run() {
+                Bitmap bitmap = getBitmap();
+                if (bitmap != null) {
+                    getScreenshotAsURI(bitmap, mQuality);
+                }
+                synchronized (this) { this.notify(); }
+            }
+        };
+
+        synchronized (r) {
+            super.cordova.getActivity().runOnUiThread(r);
+            try{
+                r.wait();
+            } catch (InterruptedException e){
+                mCallbackContext.error(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        // starting on ICS, some WebView methods
+        // can only be called on UI threads
+        mCallbackContext = callbackContext;
+        mAction = action;
+        mArgs = args;
+
+        if (action.equals("saveScreenshot")) {
+            if(PermissionHelper.hasPermission(this, PERMISSIONS[0])) {
+                saveScreenshot();
+            } else {
+                PermissionHelper.requestPermissions(this, SAVE_SCREENSHOT_SEC, PERMISSIONS);
+            }
+            return true;
+        } else if (action.equals("getScreenshotAsURI")) {
+            getScreenshotAsURI();
+            return true;
+        } else if (action.equals("getScreenshotAsURISync")){
+            getScreenshotAsURISync();
+            return true;
+        }
+        callbackContext.error("action not found");
+        return false;
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException
+    {
+        for(int r:grantResults)
+        {
+            if(r == PackageManager.PERMISSION_DENIED)
+            {
+                mCallbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
+                return;
+            }
+        }
+        switch(requestCode)
+        {
+            case SAVE_SCREENSHOT_SEC:
+                saveScreenshot();
+                break;
+        }
+    }
+
+
 }
